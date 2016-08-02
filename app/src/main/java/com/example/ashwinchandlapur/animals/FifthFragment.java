caeb7e2cf231026a622b555bfa24785cfe5befd1
AshwinChandlapur <ashwinyga@rocketmail.com>
2016-08-03 00:09:26 +0530

    Pet Animals Created

---
+++ app/src/main/java/com/example/ashwinchandlapur/animals/FifthFragment.java
@@ -0,0 +1,85 @@
+package com.example.ashwinchandlapur.animals;
+
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Example about replacing fragments inside a ViewPager. I'm using
+ * android-support-v7 to maximize the compatibility.
+ *
+ * @author Dani Lao (@dani_lao)
+ *
+ */
+public class FifthFragment extends Fragment {
+    TextView t;
+    private static final String TAG = "SecondFragment";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater
+                .inflate(R.layout.fifth_fragment, container, false);
+
+        t=(TextView)view.findViewById(R.id.tv);
+
+        Typeface myFont = Typeface.createFromAsset(getActivity().getAssets(),"fonts/Kaushan.otf");
+        t.setTypeface(myFont);
+        ImageButton btn=(ImageButton)view.findViewById(R.id.btn);
+        ImageButton btn1=(ImageButton)view.findViewById(R.id.btn1);
+        // Button btn = (Button) view.findViewById(R.id.btn);
+        final MediaPlayer sound= MediaPlayer.create(view.getContext(),R.raw.macaw);
+        sound.start();
+
+        //Button btn = (Button) view.findViewById(R.id.btn);
+
+        btn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction trans = getFragmentManager()
+                        .beginTransaction();
+                trans.replace(R.id.root_frame, new SixthFragment());
+                sound.stop();
+                trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                trans.addToBackStack(null);
+                trans.commit();
+            }
+        });
+
+        btn1.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction trans = getFragmentManager()
+                        .beginTransaction();
+				/*
+				 * IMPORTANT: We use the "root frame" defined in
+				 * "root_fragment.xml" as the reference to replace fragment
+				 */
+                trans.replace(R.id.root_frame, new FourthFragment());
+                sound.stop();
+				/*
+				 * IMPORTANT: The following lines allow us to add the fragment
+				 * to the stack and return to it later, by pressing back
+				 */
+                trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                trans.addToBackStack(null);
+
+                trans.commit();
+            }
+        });
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/ashwinchandlapur/animals/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.ashwinchandlapur.animals;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.Window;
+
+/**
+ * Example about replacing fragments inside a ViewPager. I'm using
+ * android-support-v7 to maximize the compatibility.
+ *
+ * @author Dani Lao (@dani_lao)
+ *
+ */
+public class MainActivity extends ActionBarActivity {
+
+    // For this example, only two pages
+    static final int NUM_ITEMS = 2;
+
+    ViewPager mPager;
+    SlidePagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+      //  this.supportrequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+		/* Instantiate a ViewPager and a PagerAdapter. */
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new SlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
+    }
+
+    /* PagerAdapter class */
+    public class SlidePagerAdapter extends FragmentPagerAdapter {
+        public SlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+			/*
+			 * IMPORTANT: This is the point. We create a RootFragment acting as
+			 * a container for other fragments
+			 */
+            if (position == 0)
+                return new RootFragment();
+            else
+                return new StaticFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+    }
+    public void home(View v)
+    {
+        Intent intent=new Intent(MainActivity.this,ScrollingActivity.class);
+        startActivity(intent);
+    }
+}
